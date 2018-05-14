49a7c9ce8cef19faaa7c6a26dc566c11a4b91bf6
jcaravaggio <joshc@impulsetechnologies.com>
2018-05-14 09:26:41 -0400

    classes added

---
+++ Models/SaleDay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reportApp.Models
+{
+    public class SaleDay
+    {
+        public string salesPerson;
+        public DateTime date;
+        public List<Sale> saleList;
+        public float totalVolume;
+        public float totalGP;
+
+
+        public List<Sale> retrieveSales() {
+
+
+
+
+        }
+
+        public float computeVolume()
+        {
+
+
+
+
+        }
+        public float computeGP()
+        {
+
+
+
+
+        }
+
+    }
+}
